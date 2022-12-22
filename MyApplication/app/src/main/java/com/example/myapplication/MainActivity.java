fc065795fd77f6b8e93b4b0d9b051cf59f1a9a2c
albercha17 <albercha17@gmail.com>
2022-12-22 14:55:10 +0100

    pantalla menu principal iniciada

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,37 +4,36 @@ import static com.example.myapplication.R.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imagen;
+    Button btn_cargarImg, btn_salir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_main);
 
-        imagen=(ImageView) findViewById(R.id.imagenId);
-    }
-    public void onClick(View view){
-        cargarImagen();
-    }
-    private void cargarImagen(){
-        Intent intent= new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/");
-        startActivityForResult(intent.createChooser(intent,"Seleccione la app"),10);
-    }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(resultCode== RESULT_OK){
-            Uri path= data.getData();
-            imagen.setImageURI(path);
-        }
+        btn_cargarImg=(Button) findViewById(R.id.irPagCargarImagen);
+        btn_salir=(Button) findViewById(R.id.salirBtn);
+
+        btn_cargarImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity. this, cargarFotoMenu.class);
+
+                startActivity(intent);
+            }
+        });
+
+        btn_salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
