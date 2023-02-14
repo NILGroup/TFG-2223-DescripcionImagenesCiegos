80d7905924eef70af1e4ba21084dad3d516dde1d
albercha17 <albercha17@gmail.com>
2023-02-14 15:36:24 +0100

    app.py

---
+++ webapp/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    imagenes=[ {
+                "nombre": "img1",
+                "descripcion": "Es un gato"
+                },
+               {
+                "nombre":"img2",
+                "descripcion": "nada"
+               }
+            ]
+    return jsonify(imagenes)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
